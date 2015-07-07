300ba60a012c79b2c6f267e4fc63891ad18c9b7f
Shota Aizawa <someone@nowhere.com>
2015-07-07 16:31:02 -0400

    Developed UI
    Created a new project to make you calculate unit conversion. Developed
    first version of user interface

---
+++ Unit_Conversion/Unit_Conversion/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Unit_Conversion
+//
+//  Created by 相澤渉太 on 2015/07/07.
+//  Copyright (c) 2015年 Shota Aizawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
